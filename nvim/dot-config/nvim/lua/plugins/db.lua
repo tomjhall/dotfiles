cc5a74f1e5a3c5241dc0dcfb6c9c3ac628444d80
Tom Hall <tom@arenaclub.com>
2024-06-07 09:46:07 +0100

    Fix code completion issue, add <leader>dB to toggle dbUI

--- nvim/dot-config/nvim/lua/plugins/db.lua
+++ nvim/dot-config/nvim/lua/plugins/db.lua
@@ -1,5 +1,24 @@
 return {
-  { "tpope/vim-dadbod" },
-  { "kristijanhusak/vim-dadbod-ui" },
-  { "kristijanhusak/vim-dadbod-completion" },
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  keys = {
+    {
+      "<leader>dB",
+      ":DBUIToggle<cr>",
+      desc = "Toggle Database UI",
+    },
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
 }
