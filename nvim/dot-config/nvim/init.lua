979683be9989d55d4231c46b3be3c5462bcffd1f
Tom Hall <tom@arenaclub.com>
2025-06-27 11:29:53 +0100

    Add comp local db

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -3,4 +3,5 @@ require("config.lazy")
 
 vim.g.dbs = {
   local_db = "postgres://postgres:postgres@localhost:5432/postgres",
+  comp_local = "postgres://postgres:postgres@localhost:5436/postgres",
 }
