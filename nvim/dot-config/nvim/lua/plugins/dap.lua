3d470f21339964d83d50aaa0c2fbb28439510e90
Tom Hall <tom@arenaclub.com>
2025-11-05 12:55:21 +0000

    Update dap hotkeys
    dh goes to current debugger line
    do steps over
    dO steps out
    dw shows current variable value

--- nvim/dot-config/nvim/lua/plugins/dap.lua
+++ nvim/dot-config/nvim/lua/plugins/dap.lua
@@ -52,19 +52,31 @@ return {
       { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition" },
       { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
       { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dh", function()
+        local session = require("dap").session()
+        if session then
+          local frame = session.current_frame
+          if frame then
+            vim.cmd("edit " .. frame.source.path)
+            vim.fn.cursor(frame.line, 0)
+            vim.cmd("normal! zz")
+          end
+        end
+      end, desc = "Go to Current Debug Line" },
       { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
       { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
       { "<leader>dj", function() require("dap").down() end, desc = "Down" },
       { "<leader>dk", function() require("dap").up() end, desc = "Up" },
       { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
       { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
       { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
       { "<leader>ds", function() require("dap").session() end, desc = "Session" },
       { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
       { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+      { "<leader>dw", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+      { "K", function() require("dap.ui.widgets").hover() end, desc = "Hover Variables (Debug)", ft = {"typescript", "javascript"}, mode = "n" },
     },
 
     config = function()
